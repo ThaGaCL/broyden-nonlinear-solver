f8fce765a03c40b8bb14daa4eac0b43bcfedaf28
ThaGaCL <tgcl21@inf.ufpr.br>
2026-05-25 22:04:13 -0300

    GS SOA results collected

--- srcs/non_linear.c
+++ srcs/non_linear.c
@@ -180,9 +180,9 @@ void newton(real_t* X, real_t epsilon, lint_t max_it, lint_t n, FILE* out_file)
         liberaVetor(fx);
         liberaTridiagonalSOA(jac);
         
-        free(newton_marker);
-        free(jac_marker);
-        free(linear_marker);
+        // free(newton_marker);
+        // free(jac_marker);
+        // free(linear_marker);
     });
 
     imprimeTempos(newton_elapsed_time, jac_total_elapsed_time, linear_total_elapsed_time, out_file);
